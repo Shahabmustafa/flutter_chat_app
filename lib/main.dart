88a541492ed7432df41229fb9bb6d6c022e49ec2
Shahabmustafa <shahabmustafa57@gmail.com>
2023-11-03 23:58:28 +0500

    add some code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:chats_app/src/apis/firebase_provider.dart';
-import 'package:chats_app/src/view/auth/login_screen.dart';
-import 'package:chats_app/src/view/auth/sign_up_screen.dart';
-import 'package:chats_app/src/view/splash/splah_screen.dart';
+import 'package:chats_app/view/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -20,28 +16,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => FirebaseProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Hello Chat',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          appBarTheme: AppBarTheme(
-            centerTitle: true,
-            iconTheme: IconThemeData(
-              color: Colors.black,
-            ),
-            backgroundColor: Colors.white,
-            titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
           ),
         ),
-        home: SplashPage(),
       ),
+      home: SplashPage(),
     );
   }
 }
---
+++ lib/view/screen/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:chats_app/view/screen/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  isLogin(){
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+              child: Image.asset(
+                "images/chat.png",
+                height: 200,
+                width: 200,
+              ),
+          ),
+          Center(
+            child: Text(
+            "HELLO CHAT",
+            style: GoogleFonts.acme(
+                fontSize: 35,
+            ),
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
